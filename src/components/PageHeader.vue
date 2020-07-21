b44003a5fbfc99c9d3438bd49d6ba44288cb1304
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-21 23:29:31 +0300

    path's for logo changed

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,5 +1,5 @@
 <template lang="pug" functional>
   .page-header
     .logo
-      img(srcset="//img.tyt.by/i/logo-tutby.png, //img.tyt.by/i/logo-tutby@1.5x.png 1.5x, //img.tyt.by/i/logo-tutby@2x.png 2x", src="//img.tyt.by/i/logo-tutby.png")
+      img(srcset="https://img.tyt.by/i/logo-tutby.png, https://img.tyt.by/i/logo-tutby@1.5x.png 1.5x, https://img.tyt.by/i/logo-tutby@2x.png 2x", src="https://img.tyt.by/i/logo-tutby.png")
 </template>
