6c602d68ce5580d7c6a27b8b96164a9398bf63d0
Valera Katskel <ne.vse.srazu@gmail.com>
2020-07-27 23:24:02 +0300

    report item's header fix

--- src/components/ReportItemHeader.vue
+++ src/components/ReportItemHeader.vue
@@ -1,7 +1,7 @@
 <template lang="pug" functional>
   .report-header
     .title {{props.content.title}}
-    .icon
+    .icon-holder
       svg.icon
         use(v-bind:xlink:href="props.content.icon")
 </template>
