9e1e91c837ebfa84a093608515ab871b9f930514
Valera Katskel <ne.vse.srazu@gmail.com>
2020-09-14 14:45:12 +0300

    Html entities render added, video render added for 'Titles rotation' section

--- src/components/ArticleReportItem.vue
+++ src/components/ArticleReportItem.vue
@@ -3,9 +3,10 @@
     .data-section(v-for='section in props.content' :key='section.title')
       .section-item(v-for='sc in section' :key='sc.title')
         .section-item-holder
-          .item-title {{sc.title}}
-          .item-image 
-            img(:src="sc.image")
+          .item-title(v-html="sc.title")
+          .item-media 
+            video(v-if="sc.is_video" :src="sc.media")
+            img(v-else :src="sc.media")
           
         .section-footer 
           .report-param(v-for='param in sc.params' :key="param.name")
